d70dd41651548f3ae2885ab7459488ccf88f5dff
Stephen Tramer <169061+sptramer@users.noreply.github.com>
2021-05-04 15:37:48 -0700

    Updated API reference.
    Signed-off-by: Stephen Tramer <169061+sptramer@users.noreply.github.com>

--- content/docs/user-guide/networking/_index.md
+++
@@ -1,8 +0,0 @@
----
-title: Multiplayer and networking
-date: 2021-03-02T00:23:55-05:00
-weight: 1100
----
-
-{{< preview-migrated >}}
-
--- static/docs/api/gems/LyShine/menudata.js
+++ static/docs/api/gems/LyShine/menudata.js
@@ -25,4 +25,25 @@ var menudata={children:[
 {text:"Classes",url:"annotated.html",children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
-{text:"Class Hierarchy",url:"hierarchy.html"}]}]}
+{text:"Class Hierarchy",url:"hierarchy.html"},
+{text:"Class Members",url:"functions.html",children:[
+{text:"All",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"c",url:"functions.html#index_c"},
+{text:"d",url:"functions.html#index_d"},
+{text:"g",url:"functions.html#index_g"},
+{text:"l",url:"functions.html#index_l"},
+{text:"m",url:"functions.html#index_m"},
+{text:"r",url:"functions.html#index_r"},
+{text:"s",url:"functions.html#index_s"},
+{text:"~",url:"functions.html#index__7E"}]},
+{text:"Functions",url:"functions_func.html",children:[
+{text:"a",url:"functions_func.html#index_a"},
+{text:"c",url:"functions_func.html#index_c"},
+{text:"d",url:"functions_func.html#index_d"},
+{text:"g",url:"functions_func.html#index_g"},
+{text:"l",url:"functions_func.html#index_l"},
+{text:"r",url:"functions_func.html#index_r"},
+{text:"s",url:"functions_func.html#index_s"},
+{text:"~",url:"functions_func.html#index__7E"}]},
+{text:"Variables",url:"functions_vars.html"}]}]}]}
