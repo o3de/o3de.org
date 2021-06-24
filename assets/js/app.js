86f1786e6de9c1603630f8a15af724366de1d2c8
Celeste Horgan <celeste@cncf.io>
2021-06-24 14:13:28 -0700

    implement lunr.js for search (#576)
    Signed-off-by: Celeste Horgan <celeste@cncf.io>

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,7 +32,7 @@ $(function() {
   $('.currentLi').children('i').addClass("fa-chevron-down");
 
   $('.docs-sidebar').animate({
-    scrollTop: ($('.currentPage').first().offset().top - 75)
+    //scrollTop: ($('.currentPage').first().offset().top - 75)
   },500);
 });
 
@@ -51,3 +51,61 @@ $(function() {
 });
 
 
+// Search 
+
+
+function displayResults (results, store) {
+  const resultsContainer = document.getElementById('search-results')
+  const mainContainer = document.getElementById('primary')
+  const searchResults = document.getElementById('results')
+  if (results.length) {
+    let resultList = ''
+    // Iterate and build result list elements
+    for (const n in results) {
+      const item = store[results[n].ref]
+      resultList += '<li><p><a href="' + item.url + '">' + item.title + '</a></p>'
+      resultList += '<p>' + item.content.substring(0, 400) + '...</p></li>'
+    }
+    searchResults.innerHTML = resultList
+  } else {
+    searchResults.innerHTML = 'No results found.'
+  }
+
+  resultsContainer.style.display = "block"
+  mainContainer.style.display = "none"
+}
+
+// Get the query parameter(s)
+const params = new URLSearchParams(window.location.search)
+const query = params.get('query')
+
+// Perform a search if there is a query
+if (query) {
+  // Retain the search input in the form when displaying results
+  document.getElementById('search-input').setAttribute('value', query)
+
+  const idx = lunr(function () {
+    this.ref('id')
+    this.field('title', {
+      boost: 15
+    })
+    this.field('tags')
+    this.field('content', {
+      boost: 10
+    })
+
+    for (const key in window.store) {
+      this.add({
+        id: key,
+        title: window.store[key].title,
+        tags: window.store[key].category,
+        content: window.store[key].content
+      })
+    }
+  })
+
+  // Perform the search
+  const results = idx.search(query)
+  // Update the list with results
+  displayResults(results, window.store)
+}
